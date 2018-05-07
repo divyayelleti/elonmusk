b86d60bcc746a50cd70334b5c4f73e16395a6053
divyayelleti <yelletidivya1293@gmail.com>
2018-05-07 10:48:55 +0530

    Initial commit

---
+++ localrepo/elonmusk/src/main/java/elonmusk/App.java
@@ -0,0 +1,8 @@
+package elonmusk;
+
+public class App {
+	public String sample() {
+		return "sample program";
+	}
+}
+	
---
+++ localrepo/elonmusk/src/test/java/elonmusk/Apptest.java
@@ -0,0 +1,19 @@
+package elonmusk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+	
+
+	public static void tearnDownAfterClass() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+
+	}
+
+}
